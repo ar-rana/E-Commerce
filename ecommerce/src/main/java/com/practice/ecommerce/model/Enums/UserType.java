393ccce4291c8ad508d22d08fc000c8a1b9b0c3b
Aryan Rana <aryan.rana2005@gmail.com>
2025-01-14 21:03:55 +0530

    setup postgres and users model

---
+++ ecommerce/src/main/java/com/practice/ecommerce/model/Enums/UserType.java
@@ -0,0 +1,6 @@
+package com.practice.ecommerce.model.Enums;
+
+public enum UserType {
+    customer,
+    admin
+}
---
+++ ecommerce/src/main/java/com/practice/ecommerce/model/User.java
@@ -0,0 +1,59 @@
+package com.practice.ecommerce.model;
+
+import com.practice.ecommerce.model.Enums.UserType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class User {
+
+    public User() { }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String identifier;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", identifier='" + identifier + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
